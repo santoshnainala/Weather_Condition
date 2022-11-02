a43bc5394d9424d47243b0cf1feb92bb4264bf85
santoshnainala <51361959+santoshnainala@users.noreply.github.com>
2022-11-02 22:24:51 +0530

    Add files via upload

---
+++ Weather_condi_check.py
@@ -0,0 +1,10 @@
+temp = int(input("Please enter the temperature in celsius : \n"))
+
+if temp >= 35:
+  print("It's a Hot day here")
+
+elif temp <= 25:
+  print("It's cold out there")
+
+else:
+  print("It's a pleasant day")
